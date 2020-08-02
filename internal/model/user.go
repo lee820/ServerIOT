fe8360386dc3707dfa207cf053b4355377f887e3
lee820 <476794594@qq.com>
2020-08-02 21:26:42 +0800

    gorm 回调函数完成

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,5 +1,7 @@
 package model
 
+import "github.com/jinzhu/gorm"
+
 //User 用户模型
 type User struct {
 	*Model
@@ -11,4 +13,24 @@ type User struct {
 //TableName 获取用户表的表名
 func (u User) TableName() string {
 	return "user"
+}
+
+//Create 创建用户
+func (u User) Create(db *gorm.DB) error {
+	return db.Create(&u).Error
+}
+
+//Update 更新用户信息
+func (u User) Update(db *gorm.DB, values interface{}) error {
+	return db.Model(&u).Where("id = ?", u.ID).Update(values).Error
+}
+
+//Delete 删除用
+func (u User) Delete(db *gorm.DB) error {
+	return db.Where("id = ?", u.ID).Delete(&u).Error
+}
+
+//Query 使用手机号查询用户是否存在
+func (u User) Query(db *gorm.DB, phoneNo string) error {
+	return db.Where("phone = ?", u.Phone).Find(&u).Error
 }
